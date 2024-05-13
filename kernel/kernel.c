c86a647eb9c35d33ea592ae0776bac15c5bbe169
epoll-reactor-2 <>
2024-05-13 16:07:51 +0200

    VGA buffer rotation.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,9 +4,10 @@
 #include "vga.h"
 #include "lib/stdlib.h"
 
-void krpint(const char* data)
+void kprint(const char* data)
 {
-	vga_write(data, strlen(data));
+	while (*data)
+		vga_putchar(*data++);
 }
 
 void kernel_main(void)
@@ -15,5 +16,33 @@ void kernel_main(void)
 	vga_init();
 
 	/* Newline support is left as an exercise. */
-	krpint("Hello, kernel World\nNewline\nNewline 2");
+	kprint("Hello world\n");
+	kprint("Message 1\n");
+	kprint("Message 2\n");
+	kprint("Message 3\n");
+	kprint("Message 4\n");
+	kprint("Message 5\n");
+	kprint("Message 6\n");
+	kprint("Message 7\n");
+	kprint("Message 8\n");
+	kprint("Message 9\n");
+	kprint("Message __10\n");
+	kprint("Message 11\n");
+	kprint("Message __12\n");
+	kprint("Message 13\n");
+	kprint("Message __14\n");
+	kprint("Message 15\n");
+	kprint("Message __16\n");
+	kprint("Message 17\n");
+	kprint("Message ______18\n");
+	kprint("Message 19\n");
+	kprint("Message _____20\n");
+	kprint("Message 21\n");
+	kprint("Message ____22\n");
+	kprint("Message 23\n");
+	kprint("Message ___24\n");
+	kprint("Message 25\n");
+	kprint("Message __26\n");
+	kprint("Message 27\n");
+	kprint("Message __28\n");
 }
